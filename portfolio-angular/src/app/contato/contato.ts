b7e35ab6f80cfe70c3b1bf6627ca0af1b98b42a6
maciel <20241ctb0100023@estudantes.ifpr.edu.br>
2026-06-08 20:17:52 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

---
+++ portfolio-angular/src/app/contato/contato.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+@Component({
+  selector: 'app-contato',
+  imports: [MatCardModule],
+  templateUrl: './contato.html',
+  styleUrl: './contato.css',
+})
+export class Contato {}
---
+++ portfolio-angular/src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+
+@Component({
+selector: 'app-home',
+imports: [MatCardModule],
+templateUrl: './home.html',
+styleUrl: './home.css'
+})
+
+export class Home {}
